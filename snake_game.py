0bb83ee8b20a2d61e98e5bed5bcca55cdb2d83d0
Yotam <yotam.aflalo433@gmail.com>
2023-06-08 15:31:37 +0300

    add snake to snake game and update moves

--- snake_game.py
+++ snake_game.py
@@ -1,6 +1,6 @@
 from typing import Optional
 from game_display import GameDisplay
-
+from new_class_try import snake, vertebra
 class SnakeGame:
 
     def __init__(self) -> None:
@@ -23,16 +23,20 @@ class SnakeGame:
 
     def update_objects(self)-> None:
         if (self.__key_clicked == 'Left') and (self.__x > 0) and (self.__pre_move != "Right"):
-            self.__x -= 1
+            self.__snake.move_snake_left()
+            #self.__x -= 1
             self.__pre_move = 'Left'
         elif (self.__key_clicked == 'Right') and (self.__x < 40) and (self.__pre_move != "Left"):
-            self.__x += 1
+            self.__snake.move_snake_right()
+            #self.__x += 1
             self.__pre_move = 'Right'
         elif (self.__key_clicked == 'Up') and (self.__y < 30) and (self.__pre_move != "Down"):
-            self.__y +=1
+            #self.__y +=1
+            self.__snake.move_snake_up()
             self.__pre_move ="Up"
         elif (self.__key_clicked == 'Down') and (self.__y > 0) and (self.__pre_move != "Up"):
-            self.__y -=1
+            #self.__y -=1
+            self.__snake.move_snake_down()
             self.__pre_move = "Down"
     def draw_board(self, gd: GameDisplay) -> None:
         gd.draw_cell(self.__x, self.__y, "blue")
