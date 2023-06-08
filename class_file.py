eb29b695ac0de43181df803faa0e77781f128423
ASUS <Almog.Laufer@mail.huji.ac.il>
2023-06-08 12:03:56 +0300

    class file

--- class_file.py
+++ class_file.py
@@ -1,21 +1,56 @@
 
-
 class Vertebra:
     """
     An object that contains information about the presence of the next vertebra in the snake.
     The Vertebra can be formed from the last link.
-
     """
-    def __init__(self, num, orien_turn):#צריך להחליט אם כל חוליה שומרת כיוון או פניה,כלומר האם החוליה יודעת מה הכיוון שלה או רק אם עליה לבצע פניה.
-        self.num = num                 #על כן השם המצחיק
-        self.orien_turn = orien_turn
+    def __init__(self, num, loc, next = None):
+        """
+        :param num: The number of the link in the chain.
+        :param orientation:The direction the chain continues.
+        0-3 so that within modulo 4 turning left is always minus 1 and turning right is plus 1.
+        regardless of the absolute direction.
+        :param next: the nex vertebra
+        """
+        self.num = num
+        self.loc = loc
+        if next != None:
+            self.next = Vertebra(next)
+        else:
+            self.next = None
+
+
+    def add_vertebra_link(self):
+        if self.next == None:
+            self.next = Vertebra(self.num+1, self.orientation)
+        else:
+            self.next.add_vertebra_link()
+
+
+    def chain_lenth(self):
+        if self.next == None:
+            return self.num
+        else:
+            return self.next.chain_lnth()
+
+    def pass_to_next(self,):
 
 
 
 class Snake:
-    pass
+
+    def __int__(self,height,width):
+        self.head = SnakeHead(tuple(height//2 ,width//2))
+
+
 class SnakeHead:
-    pass
+    """
+    The head of the serpent holds the first link.
+    All actions from the game and to the vertebras will be performed on it.
+    """
+    def __init__(self, loc: tuple):
+        self.__loc = loc
+        self.body = Vertebra(0, loc,(1, (loc[0]-1,loc[1]),(2 ,(loc[0]-2, loc[1]))))
 
 
 
@@ -23,3 +58,4 @@ class Apple:
     pass
 class Wall:
     pass
+
