4d1609bab8551a9a9fc6624c05f5cd16ecd1f82a
Yotam <yotam.aflalo433@gmail.com>
2023-06-08 11:40:22 +0300

    move dir

--- snake_game.py
+++ snake_game.py
@@ -7,6 +7,7 @@ class SnakeGame:
         self.__x = 5
         self.__y = 5
         self.__key_clicked = None
+        self.__pre_move = None
 
     def read_key(self, key_clicked: Optional[str])-> None:
         self.__key_clicked = key_clicked
@@ -15,11 +16,18 @@ class SnakeGame:
         return self.__key_clicked
 
     def update_objects(self)-> None:
-        if (self.__key_clicked == 'Left') and (self.__x > 0):
+        if (self.__key_clicked == 'Left') and (self.__x > 0) and (self.__pre_move != "Right"):
             self.__x -= 1
-        elif (self.__key_clicked == 'Right') and (self.__x < 40):
+            self.__pre_move = 'Left'
+        elif (self.__key_clicked == 'Right') and (self.__x < 40) and (self.__pre_move != "Left"):
             self.__x += 1
-
+            self.__pre_move = 'Right'
+        elif (self.__key_clicked == 'Up') and (self.__y < 40) and (self.__pre_move != "Down"):
+            self.__y +=1
+            self.__pre_move ="Up"
+        elif (self.__key_clicked == 'Down') and (self.__y > 0) and (self.__pre_move != "Up"):
+            self.__y -=1
+            self.__pre_move = "Down"
     def draw_board(self, gd: GameDisplay) -> None:
         gd.draw_cell(self.__x, self.__y, "blue")
 
