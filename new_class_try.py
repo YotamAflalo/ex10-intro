0cd62013428593fa5ad2f34a87b3de70dc43c0a5
Yotam <yotam.aflalo433@gmail.com>
2023-06-08 15:11:52 +0300

    try - new snake add

--- new_class_try.py
+++ new_class_try.py
@@ -1,4 +1,5 @@
 class vertebra:
+
     def __init__(self,x,y, next=None, prev= None):
         self.__x = x
         self.__y = y
@@ -17,8 +18,10 @@ class vertebra:
 
 
 class snake:
+    ITEM_NOT_FOUND = -1
     def __init__(self, head=None):
-        self.__head = self.__tail = None
+        self.__head = None
+        self.__tail = None
         self.__length = 0
 
     def add_first(self, vertebra):
@@ -39,4 +42,22 @@ class snake:
         return self.__head == None
 
     def pop_lest(self):
+        pass
 
+    def __len__(self):
+        current = self.__head
+        count = 0
+        while current is not None:
+            count = count + 1
+            current = current.get_next()
+        return count
+    def collision(self, loc):
+        return self.collision_helper(self.__head, loc, 0)
+
+    def collision_helper(self, cur, loc, index):
+        if index >= self.__len__():
+            return -1
+
+        if cur.get_loc() == loc:
+            return index
+        return self.collision_helper(cur.get_next(), loc,index + 1)
