cf86e0e31b9b8d5fdc59467f3f6b431990915a26
Yotam <yotam.aflalo433@gmail.com>
2023-06-09 17:58:53 +0300

    apple and walls 17:58

--- ap_and_walls.py
+++ ap_and_walls.py
@@ -8,12 +8,22 @@ class Apples:
         self.max_num = max_num #כמות מקסימלית של תפוחים
         self.corrent_num = 0
         self.ap_locs = set() #נכניס לסט
-    def apple_generetor(self,snake=None,walls= None):
+
+
+    def apple_generetor(self,walls_loc:dict):
         new_apple_loc = get_random_apple_data() #tuple
-        if new_apple_loc not in self.ap_locs:
+        free_palce = 1
+        if new_apple_loc in self.ap_locs:
+            free_palce =0
+        for key in walls_loc.keys():
+            if new_apple_loc in walls_loc[key]:
+                free_place = 0
+        if free_palce==1:
             #כרגע הפונקציה לא מתחשבת בהתנגשות עם נחש או קיר. צריך לחשוב איפה להכניס את זה
             self.ap_locs.add(new_apple_loc)
             self.corrent_num = len(self.ap_locs)
+
+
     def apple_remover(self,loc:tuple):
         '''
         this func remove apple from the board. we will use it if
@@ -32,19 +42,6 @@ class Apples:
                 if ap_loc in wall_locs[wall]:
                     self.apple_remover(ap_loc)
 
-'''ap = Apples()
-
-for i in range(0,10):
-    if ap.need_more_apple():
-        ap.apple_generetor()
-        print(ap.ap_locs)
-        print(ap.corrent_num)
-locs = list(ap.ap_locs)
-print("locs",locs)
-for i in locs:
-    ap.apple_remover(i)
-    print(ap.ap_locs)
-    print(ap.corrent_num)'''
 
 class Walls:
     def __init__(self,max_num_walls = 2):
@@ -69,7 +66,12 @@ class Walls:
             self.num_walls+=1
 
     def wall_move(self):
-        pass
+        key_change = {'Down': [-1, 0], 'Up': [1, 0], 'Right': [0, -1], 'Left': [0, 1]}
+        for wall in self.walls_loc.keys():
+            dir = wall[-1]
+            for brick in self.walls_loc[wall]:
+                brick[0]= brick[0]+key_change[dir][0]
+                brick[1]=brick[1]+key_change[dir][1]
 
     def wall_remove(self,loc:tuple):
         self.walls_loc.pop(loc)
@@ -77,4 +79,8 @@ class Walls:
 
     def wall_locs(self):
         return self.walls_loc
+
+    def need_more_walls(self):
+        return self.num_walls<self.max_walls
+
 
