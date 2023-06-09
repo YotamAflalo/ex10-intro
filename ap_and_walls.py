68ca33b6264cfb861edc251a55bfa8b88333383c
Yotam <yotam.aflalo433@gmail.com>
2023-06-09 17:11:47 +0300

    apple and walls

--- ap_and_walls.py
+++ ap_and_walls.py
@@ -26,7 +26,11 @@ class Apples:
     def need_more_apple(self):
         return self.corrent_num<self.max_num
 
-
+    def apple_wall_collision(self,wall_locs:dict):
+        for ap_loc in self.ap_locs:
+            for wall in wall_locs.keys():
+                if ap_loc in wall_locs[wall]:
+                    self.apple_remover(ap_loc)
 
 '''ap = Apples()
 
@@ -41,3 +45,36 @@ for i in locs:
     ap.apple_remover(i)
     print(ap.ap_locs)
     print(ap.corrent_num)'''
+
+class Walls:
+    def __init__(self,max_num_walls = 2):
+        self.walls_loc = {}
+        self.max_walls =max_num_walls
+        self.num_walls =0
+
+    def wall_generetor(self,ap_locs:set,snake_locs:set):
+        x_wall,y_wall,dir_wall = get_random_wall_data()
+        key_change = {'Down': [-1, 0], 'Up': [1, 0], 'Right': [0, -1], 'Left': [0, 1]}
+        free_place = 1
+        for i in range(3):
+            new_loc = x_wall+i*key_change[dir_wall][0],y_wall+i*key_change[dir_wall][1]
+            if new_loc in ap_locs or new_loc in snake_locs:
+                free_place=0
+            for key in self.walls_loc.keys():
+                if new_loc in self.walls_loc[key]:
+                    free_place=0
+        if free_place ==1:
+            palce_list = [(x_wall+i*key_change[dir_wall][0],y_wall+i*key_change[dir_wall][1]) for i in range(3)]
+            self.walls_loc[(x_wall,y_wall)] = palce_list
+            self.num_walls+=1
+
+    def wall_move(self):
+        pass
+
+    def wall_remove(self,loc:tuple):
+        self.walls_loc.pop(loc)
+
+
+    def wall_locs(self):
+        return self.walls_loc
+
